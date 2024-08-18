08d1d7a9b9c8dac1e781b120533d4758156de370
kyungnamKo <kkn8449@gmail.com>
2024-08-18 21:50:00 +0900

    문제번호 : 42577, 날짜 : 240818

--- knko/42577.py
+++ knko/42577.py
@@ -1,15 +1,8 @@
 def solution(phone_book):
-#     cnt = [len(i) for i in phone_book]
-#     dic = {a:b for a,b in enumerate(phone_book)}
+    phone_book = sorted(phone_book)
     
-    # for i in range(len(phone_book)):
-    #     for j in range(1, )
-    #     if dic[i] 
-
-    while phone_book:
-        a = phone_book.pop(0)
-        k = [i[:len(a)] for i in phone_book if len(i) >= len(a)]
-        if a in k:
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
     
     return True
