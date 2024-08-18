a72d34372f0c2c2b01817c88937ddbff3cf6af59
Hyeong Uk Choi <wook9543@gmail.com>
2024-08-18 19:33:58 +0900

    문제번호 : 42577, 날짜 : 240818

--- hyeonguk_choi/42577.py
+++ hyeonguk_choi/42577.py
@@ -1,19 +1,14 @@
 def solution(phone_book):
     answer = True
-    lens = []
+    phone_book.sort()
+    tag = 1
     for i in phone_book:
-        lens.append(len(i))
-    lens = list(set(lens))
-    for j in lens:
-        l_dict = {}
-        for x in phone_book:
-            l_dict[x] = hash(x[0:j])
-        if (len(list(set(l_dict.values())))) < len(phone_book):
-            for y in phone_book:
-                l_dict.pop(y)
-                if hash(y) in list(l_dict.values()):
-                    answer = False
-                    break
-                else:
-                    l_dict[y] = hash(y[0:j])
+        len_val = len(i)
+        for j in phone_book[tag:tag+1]:
+            if hash(j[0:len_val]) == hash(i):
+                answer = False
+                break
+            tag += 1
+        if answer == False:
+            break
     return answer
